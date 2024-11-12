78148ed41a90f79d2df555e1909475eecdce518e
Hikmah97 <olanipekunhikmahkikelomo@gmail.com>
2024-11-12 11:08:00 +0100

    js challenge day one

---
+++ dayone.js
@@ -0,0 +1,53 @@
+// Comments can make code readable
+// Welcome to 30DaysOfJavascript
+// Comments can make code readable, easy to reuse and informative.
+
+// DATA TYPES
+let ageMale = 24;  // number
+const PI = 3.14;  // number
+let firstName = "Hikmah"; // String
+let isSingle = false; // Boolean
+let number = null; // Null
+const count = undefined; // Undefined
+
+// TYpeOf Data Types
+console.log(typeof"Faruk");
+console.log(typeof 9);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof false);
+
+// Declare Variables without assigning values
+// let
+// var
+// const
+// let
+
+// Declare Variables with assigning values
+let year = 34;
+// var countryCapital = "Abuja";
+const boilingPoint = 100;
+// let $currency_ = America;
+
+// Variables to store personal information
+let first_name = "Hikmah";
+let last_name = "Olanipekun";
+let marital_status = "Married";
+let country = "Nigeria";
+let age = 27;
+
+// Declare variables to store your first name, last name, marital status, country and age in a multiple line
+console.log(first_name);
+console.log(last_name);
+console.log(marital_status);
+console.log(country);
+console.log(age);
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+console.log(first_name, last_name, marital_status, country, age);
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 27;
+let yourAge = 29;
+console.log("I am", myAge, "years old.");
+console.log("You are", yourAge, "years old.");
---
+++ script.js
@@ -0,0 +1,43 @@
+let age = 27;
+let year = 1997;
+console.log(age, year);
+
+const points = 199;
+// const address = Lagos;
+// const job = Software;
+
+console.log(points);
+
+// document.innerHTML
+document.getElementById("try").innerHTML = "This is my first trial outside class"
+document.write(33+33+33);
+
+// alert
+// alert("Hello, World");
+// window.alert(2+2);
+
+// String Concatenation
+let firstName = "Hikmah";
+let lastName = "Olanipekun";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// getting characters
+console.log(lastName[5]);
+
+// String Length
+console.log(fullName.length);
+console.log(lastName.length);
+console.log(firstName.toLowerCase());
+console.log(fullName.toUpperCase());
+
+let email = "olanipekunhikmahkikelomo@gmail.com"
+let index = email.indexOf('@');
+console.log(index);
+
+// Checking Data Types
+console.log(typeof "Hikmah");
+console.log(typeof 4);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
